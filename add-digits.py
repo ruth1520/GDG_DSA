d3a72ad3debd383476aeb320aada7d2b0cbda43d
rutha23 <Ruthephrem1520@gmail.com>
2025-12-20 22:40:48 +0300

    Implement addDigits method to sum digits of a number

---
+++ add-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        number = str(num)
+        while len(number) > 1:
+            sumtot = 0                
+            for i in number:          
+                sumtot += int(i)
+            number = str(sumtot)      
+        return int(number)
